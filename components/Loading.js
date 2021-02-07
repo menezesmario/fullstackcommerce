850a08b804dac2fd95d00627799ac516779ee553
Mário Menezes <mario082@gmail.com>
2021-02-07 20:49:44 -0300

    Loading 1

--- components/Loading.js
+++ components/Loading.js
@@ -1,5 +1,14 @@
 const Loading = () => {
-    return <h2>Loading....</h2>
+    return (
+        <div className="position-fixed w-100 h-100 text-center loading"
+        style={{background: '#0008', color: 'white', top: 0, left: 0, zIndex: 9 }}>
+            <svg width="205" height="250" viewBox= "0 0 40 50">
+                <polygon strokeWidth="1" stroke="#fff" fill="none"
+                points="20,1 40,40 1,40"> </polygon>
+                </svg>
+
+        </div>
+        )
 }
 
 export default Loading
