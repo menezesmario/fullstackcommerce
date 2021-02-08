a53cdca52c528d1f8be1796e0b3df0bb053852b7
Mário Menezes <mario082@gmail.com>
2021-02-08 20:59:52 -0300

    access token

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     env: {
         "BASE_URL": "http://localhost:3000",
-        "MONGODB_URL": "mongodb+srv://mario:Goprohero5@cluster0.6dafq.mongodb.net/fullcommerce?retryWrites=true&w=majority"
+        "MONGODB_URL": "mongodb+srv://mario:Goprohero5@cluster0.6dafq.mongodb.net/fullcommerce?retryWrites=true&w=majority",
+        "ACCESS_TOKEN_SECRET": "*H8VB'fV_:XC]3v[.rE$.aE,7Y}n{Vb?SXwH5aj6",
+        "REFRESH_TOKEN_SECRET": "`25'<y%ps}_gNp=<>h6#(*5,^QDPeWX{3tYq'Zv$kv()!9`>2D[byF9DX3m[EHMx=&[<9}R%JA89<Ab'"
     }
 }
