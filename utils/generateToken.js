0a4544be0602e23418e65d5fb8c5f7eb3383d60b
Mário Menezes <mario082@gmail.com>
2021-02-08 21:03:31 -0300

    generateToken

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,1 +1,9 @@
-import 
+import jwt from 'jsonwebtoken'
+
+export const createAccessToken = (payload) => {
+    return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '15m'})
+}
+
+export const createRefreshToken = (payload) => {
+    return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '7d'})
+}
